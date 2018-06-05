14c9c8d0ad59568ab413a36580d2a67ad8eda0ef
Jacek Boguta <jacekboguta@gmail.com>
2018-06-05 17:45:00 +0200

    Zmiana nazw klas

--- carcalclib/src/main/java/pl/wrzosdev/Statistics.java
+++ carcalclib/src/main/java/pl/wrzosdev/Statistics.java
@@ -10,7 +10,7 @@ public class Statistics {
     static RepairCalc repairCalc = new RepairCalc();
 
     public static void initFuel(ArrayList<FuelTank> fuelHistory) { Statistics.fuelCalc.fuelHistory = fuelHistory; }
-    public static void initRepairs(ArrayList<Repairs> repairsHistory) { Statistics.repairCalc.repairHistory = repairsHistory; }
+    public static void initRepairs(ArrayList<Repairs> repairsHistory) { Statistics.repairCalc.repairsHistory = repairsHistory; }
 
     public static FuelCalc getFuelCalc() {
         if (fuelCalc.fuelHistory.isEmpty()) {
@@ -20,7 +20,7 @@ public class Statistics {
         }
     }
     public static RepairCalc getRepairCalc() {
-        if (repairCalc.repairHistory.isEmpty()) {
+        if (repairCalc.repairsHistory.isEmpty()) {
             throw new IllegalStateException("Nie zainicjalizowałeś historii tankowania!");
         } else {
             return repairCalc;
