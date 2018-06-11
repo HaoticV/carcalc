a5b19629cec8912ed5d253e10475e5bdb9c84a3b
Jacek Boguta <jacekboguta@gmail.com>
2018-06-11 13:01:15 +0200

    #16 close Ile w sumie wydałeś na wszystkie naprawy #22 close klasy narzędziowe żeby były osobne do wydatku, tankowania, naprawy #21 close Dodać klasę testów do napraw #20 close Dostosować kod do wydzielonej zmiany

--- carcalclib/src/main/java/pl/wrzosdev/RepairCalc.java
+++ carcalclib/src/main/java/pl/wrzosdev/RepairCalc.java
@@ -8,10 +8,6 @@ public class RepairCalc {
     ArrayList<Repair> repairHistory = new ArrayList<>();
 
     public int sumCostOfAllRepairs() {
-            int sum = 0;
-            for(int i = 0; i<= repairHistory.size()-1; i++) {
-                sum += repairHistory.get(i).cost;
-            }
-            return sum;
+            return SumUtils.allCostRepairs(repairHistory);
         }
 }
