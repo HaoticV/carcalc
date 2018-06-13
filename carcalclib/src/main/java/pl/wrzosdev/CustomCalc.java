492da79b64ce2f43eb6e770f4ba342cfe48a528d
krzys <k.ciolek93@gmail.com>
2018-06-13 14:36:13 +0200

    #14 Miesięczny koszt napraw- średnio

--- carcalclib/src/main/java/pl/wrzosdev/CustomCalc.java
+++ carcalclib/src/main/java/pl/wrzosdev/CustomCalc.java
@@ -3,7 +3,9 @@ package pl.wrzosdev;
 import pl.wrzosdev.model.FuelTank;
 import pl.wrzosdev.model.Repair;
 
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class CustomCalc {
     ArrayList<Repair> repairHistory = new ArrayList<>();
@@ -11,5 +13,19 @@ public class CustomCalc {
 
     public int FuelAndRepairCosts() {
         return SumUtils.allCostRepairs(repairHistory) + SumUtils.allCostFuel(fuelHistory);
+    }
+
+    public double monthCostRepairs() {
+        //znajdź datę pierwszego wpisu w aplikacji(tankowanie lub naprawa)
+        long firstdate = fuelHistory.get(0).date.getTime();
+        if (repairHistory.get(0).date.getTime() < fuelHistory.get(0).date.getTime()) {
+            firstdate = repairHistory.get(0).date.getTime();
+        }
+        long secdate = fuelHistory.get(fuelHistory.size() - 1).date.getTime();
+        if (repairHistory.get(repairHistory.size() - 1).date.getTime() > fuelHistory.get(fuelHistory.size() - 1).date.getTime()) {
+            secdate = repairHistory.get(repairHistory.size() - 1).date.getTime();
+        }
+        long time = TimeUnit.MILLISECONDS.toDays(secdate - firstdate);
+        return (double) SumUtils.allCostRepairs(repairHistory) / time * 30;
     }
 }
