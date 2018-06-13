fca2c37e73bac34a11932c71a03f67b3c0271da9
krzys <k.ciolek93@gmail.com>
2018-06-13 16:47:23 +0200

    #15 Jak często jest naprawa

--- carcalclib/src/main/java/pl/wrzosdev/RepairCalc.java
+++ carcalclib/src/main/java/pl/wrzosdev/RepairCalc.java
@@ -4,6 +4,7 @@ import pl.wrzosdev.model.Repair;
 
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class RepairCalc {
     ArrayList<Repair> repairHistory = new ArrayList<>();
@@ -12,4 +13,8 @@ public class RepairCalc {
         return SumUtils.allCostRepairs(repairHistory);
     }
 
+    public float frequencyRepair() {
+        long days = TimeUnit.MILLISECONDS.toDays(repairHistory.get(repairHistory.size() - 1).date.getTime() - repairHistory.get(0).date.getTime());
+        return (float) days / repairHistory.size();
+    }
 }
