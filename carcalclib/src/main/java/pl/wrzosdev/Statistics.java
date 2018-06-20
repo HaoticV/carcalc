6903241bc173cd11761a185cb0a44ed111b2ab09
Jacek Boguta <jacekboguta@gmail.com>
2018-06-20 15:15:47 +0200

    Gruby refaktor- nowa struktura kosztów, wydzielenie pakietów, uproszczenie inicjalizacji

--- carcalclib/src/main/java/pl/wrzosdev/Statistics.java
+++ carcalclib/src/main/java/pl/wrzosdev/Statistics.java
@@ -1,9 +1,13 @@
 package pl.wrzosdev;
 
-import pl.wrzosdev.model.Cost;
+import pl.wrzosdev.costs.CostCalc;
+import pl.wrzosdev.costs.custom.CustomCalc;
+import pl.wrzosdev.costs.fuel.FuelCalc;
+import pl.wrzosdev.costs.repair.RepairCalc;
 import pl.wrzosdev.model.CustomCost;
 import pl.wrzosdev.model.FuelCost;
 import pl.wrzosdev.model.RepairCost;
+import pl.wrzosdev.model.costs.Cost;
 
 import java.util.ArrayList;
 
@@ -12,14 +16,27 @@ public class Statistics {
     static RepairCalc repairCalc;
     static CustomCalc customCalc;
 
+    static CostCalc costCalc;
+
     public static void init(ArrayList<Cost> costsHistory) {
+        initAllCosts(costsHistory);
+        initSpecificCosts(costsHistory);
+    }
+
+    private static void initSpecificCosts(ArrayList<Cost> costsHistory) {
         fuelCalc = new FuelCalc();
         repairCalc = new RepairCalc();
         customCalc = new CustomCalc();
         for (Cost cost : costsHistory) {
             if (cost instanceof FuelCost) fuelCalc.fuelHistory.add((FuelCost) cost);
             if (cost instanceof RepairCost) repairCalc.repairHistory.add((RepairCost) cost);
+            if (cost instanceof CustomCost) customCalc.customHistory.add((CustomCost) cost);
         }
+    }
+
+    private static void initAllCosts(ArrayList<Cost> costsHistory) {
+        costCalc = new CostCalc();
+        costCalc.costHistory.addAll(costsHistory);
     }
 
     public static FuelCalc getFuelCalc() {
---
+++ carcalclib/src/main/java/pl/wrzosdev/costs/SumUtils.java
@@ -0,0 +1,43 @@
+package pl.wrzosdev.costs;
+
+import pl.wrzosdev.model.FuelCost;
+import pl.wrzosdev.model.costs.Cost;
+
+import java.util.ArrayList;
+
+public class SumUtils {
+
+    //? extends cost pozwala przyjac kolekcji dowolna klase dziedziczaca po cost
+    public static int allCosts(ArrayList<? extends Cost> costsHistory) {
+        int sumOfRefills = 0;
+        for (int i = 0; i <= costsHistory.size() - 1; i++) {
+            sumOfRefills += costsHistory.get(i).cost;
+        }
+        return sumOfRefills;
+    }
+
+    public static int allLitersSum(ArrayList<FuelCost> fuelHistory) {
+        Integer liters = 0;
+        for (FuelCost fuel : fuelHistory) {
+            liters += fuel.liters;
+        }
+        return liters;
+    }
+
+    public static int allMileageSum(ArrayList<FuelCost> fuelHistory) {
+        return fuelHistory.get(fuelHistory.size() - 1).mileage - fuelHistory.get(0).mileage;
+    }
+
+
+    public static long allTimeSum(ArrayList<? extends Cost> fuelHistory) {
+        return fuelHistory.get(fuelHistory.size() - 1).date.getTime() - fuelHistory.get(0).date.getTime();
+    }
+
+    public static int allCostRepairs(ArrayList<? extends Cost> repairCostHistory) {
+        int sum = 0;
+        for (int i = 0; i <= repairCostHistory.size() - 1; i++) {
+            sum += repairCostHistory.get(i).cost;
+        }
+        return sum;
+    }
+}
