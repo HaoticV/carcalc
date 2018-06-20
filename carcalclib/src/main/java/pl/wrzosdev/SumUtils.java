965f3d36c7fae688dd6a698c65371a12868e3d14
Jacek Boguta <jacekboguta@gmail.com>
2018-06-20 14:33:34 +0200

    konstruktory, generyki

--- carcalclib/src/main/java/pl/wrzosdev/SumUtils.java
+++ carcalclib/src/main/java/pl/wrzosdev/SumUtils.java
@@ -1,13 +1,14 @@
 package pl.wrzosdev;
 
-import pl.wrzosdev.model.FuelTank;
-import pl.wrzosdev.model.Repair;
+import pl.wrzosdev.model.Cost;
+import pl.wrzosdev.model.FuelCost;
+import pl.wrzosdev.model.RepairCost;
 
 import java.util.ArrayList;
 
 class SumUtils {
 
-    static int allCostUsedFuel(ArrayList<FuelTank> fuelHistory) {
+    static int allCostUsedFuel(ArrayList<FuelCost> fuelHistory) {
         int sumOfRefills = 0;
         for (int i = 0; i <= fuelHistory.size() - 1 - 1; i++) {
             sumOfRefills += fuelHistory.get(i).cost;
@@ -15,7 +16,8 @@ class SumUtils {
         return sumOfRefills;
     }
 
-    static int allCostFuel(ArrayList<FuelTank> fuelHistory) {
+    //? extends cost pozwala przyjac kolekcji dowolna klase dziedziczaca po cost
+    static int allCostFuel(ArrayList<? extends Cost> fuelHistory) {
         int sumOfRefills = 0;
         for (int i = 0; i <= fuelHistory.size() - 1; i++) {
             sumOfRefills += fuelHistory.get(i).cost;
@@ -23,27 +25,27 @@ class SumUtils {
         return sumOfRefills;
     }
 
-    static int allLitersSum(ArrayList<FuelTank> fuelHistory) {
+    static int allLitersSum(ArrayList<FuelCost> fuelHistory) {
         Integer liters = 0;
-        for (FuelTank fuel : fuelHistory) {
+        for (FuelCost fuel : fuelHistory) {
             liters += fuel.liters;
         }
         return liters;
     }
 
-    static int allMileageSum(ArrayList<FuelTank> fuelHistory) {
+    static int allMileageSum(ArrayList<FuelCost> fuelHistory) {
         return fuelHistory.get(fuelHistory.size() - 1).mileage - fuelHistory.get(0).mileage;
     }
 
 
-    static long allTimeSum(ArrayList<FuelTank> fuelHistory) {
+    static long allTimeSum(ArrayList<? extends Cost> fuelHistory) {
         return fuelHistory.get(fuelHistory.size() - 1).date.getTime() - fuelHistory.get(0).date.getTime();
     }
 
-    static int allCostRepairs(ArrayList<Repair> repairHistory) {
+    static int allCostRepairs(ArrayList<Cost> repairCostHistory) {
         int sum = 0;
-        for(int i=0; i<= repairHistory.size()-1; i++){
-            sum+=repairHistory.get(i).cost;
+        for (int i = 0; i <= repairCostHistory.size() - 1; i++) {
+            sum += repairCostHistory.get(i).cost;
         }
         return sum;
     }
