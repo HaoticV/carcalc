adeebab0b04183146507f75243bc4c92b9570761
wrzosdev <wrzosdev@gmail.com>
2018-06-26 13:13:32 +0200

    Sugestia parametryzowanych testów

--- carcalclib/src/test/java/pl/wrzosdev/boundaries/singles/SingleFuelTest.java
+++ carcalclib/src/test/java/pl/wrzosdev/boundaries/singles/SingleFuelTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import pl.wrzosdev.Statistics;
+import pl.wrzosdev.model.FuelCost;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -16,13 +17,14 @@ public class SingleFuelTest {
     public void setUp() throws Exception {
         DateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Statistics.init(new ArrayList<>((Arrays.asList(
-
+                new FuelCost(sdf.parse("04/04/2018"), 243450, 230, 47)
         ))));
     }
 
     @Test
     public void GivenRepairCostsWhenCommonRepairCostRequestedThenProperCostReturned(){
         Assert.assertTrue(false);
+        //todo może parametryzowane?
         // TODO: 23.06.2018 dopisać jakieś testy
     }
 }
